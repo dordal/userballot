9aa97c8baf425cffc3ce1285ca2a8f38f7cd6d37
David Ordal <david@ordal.com>
2013-09-21 15:28:13 -0700

    updated login controller

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,12 +1,12 @@
 'use strict';
 
-userballotApp.controller('LoginCtrl', function($scope, $location, angularFire) {
+userballotApp.controller('LoginCtrl', function($scope, $location, angularFire, angularFireAuth) {
     $scope.email = '';
     $scope.password = '';
 
     // authenticate a user
     var userRef = new Firebase('https://userballotdb.firebaseio.com');
-	var auth = new FirebaseSimpleLogin(userRef, function(error, user) {
+/*	var auth = new FirebaseSimpleLogin(userRef, function(error, user) {
 		if (error) {
 			console.log(error);
 			// an error occurred while attempting login
@@ -26,10 +26,35 @@ userballotApp.controller('LoginCtrl', function($scope, $location, angularFire) {
 			console.log("logged out");
 		}
 	});
+*/
+	angularFireAuth.initialize(userRef, {
+		scope: $scope, 
+		name: "user",
+		callback: function(error, user) {
+			if (error) {
+				console.log(error);
+				// an error occurred while attempting login
+				switch(error.code) {
+				  	case 'INVALID_EMAIL':
+				  		alert("invalid email address");
+				  	case 'INVALID_PASSWORD':
+				  		alert("invalid password");
+				  	default:
+				}
+			} else if (user) {
+				// user authenticated with Firebase
+				console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+				$location.path('/admin');
+			} else {
+				// user is logged out
+				console.log("logged out");
+			}
+	}});
 
+	// Function to handle login
 	$scope.login = function() {
 		console.log("Logging in with", $scope.email, $scope.password);
-		auth.login('password', {
+		angularFireAuth.login('password', {
 			email: $scope.email,
 			password: $scope.password
 		});
