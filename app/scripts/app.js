bc5f2231585e86fc30ed7953094bf41527befb12
Amy Langill <amylangill@gmail.com>
2013-09-21 10:32:05 -0700

    Initial directory structure

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var userballotApp = angular.module('userballotApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+userballotApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
