b77bc5cd4bbf9269602733e8ce051b9ce9aa925e
Dylan Gleason <dgleason@exavault.com>
2013-09-22 11:43:44 -0700

    Added validation for login page.

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -3,23 +3,30 @@
 userballotApp.controller('SignupCtrl', function($scope, $location, angularFire, angularFireAuth, userballotAuthSvc) {
     $scope.email = '';
     $scope.password = '';
-    $scope.error = null;
+    $scope.error = '';
+    $scope.urlError = '';
+    $scope.emailError = '';
 
     // authenticate a user
 	$scope.register = function() {
 		angularFireAuth.createUser($scope.email, $scope.password, function(error, user) {
-		  if (!error) {
-		  	// login the user and redirect to the admin interface
+		    var regex = /[A-Za-z0-9_.-]+@[A-Za-z0-9]+\.[a-zA-Z0-9]+/;
+		    var emailMatch = $scope.email.match(regex);
+
+		    if (!error && emailMatch) {
+			// login the user and redirect to the admin interface
 		  	// console.log("User Reg:", user);
 		  	$location.path('/admin');
-		  } else {
-		  	// The email is already in use, mabye?
-		  	if (error.code == 'EMAIL_TAKEN') {
-		  		$scope.error = "Email is already in use. Please login.";
-		  	}else {
-		  		$scope.error = "Registration Error: "  + error.code;
-		  	}
-		  }
+		    } else if (!emailMatch) {
+			$scope.emailError = " Not a valid email.";
+		    } else {
+			if (error.code === 'EMAIL_TAKEN') {
+		  	    $scope.emailError = " Email is already in use. Please login.";
+			} else {
+			    $scope.error = "Registration Error: "  + error.code;
+			}
+		    }
 		});
-	}
+	};
 });
+
