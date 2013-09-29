26194639685dd1a3d27453d0e6530050f1dec20b
Darren Buckner <darren+git@darrenbuckner.com>
2013-09-28 23:16:39 -0700

    adding 'how it works' content to dashboard

--- app/scripts/ui-interactions.js
+++ app/scripts/ui-interactions.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-	
-	$('body').delegate('.main-nav-item', 'click', function(e) {
+	$('body').delegate('#helpBlockShow', 'click', function(e) {
 		e.preventDefault();
 		if ($(this).hasClass('open')) {
 			$(this).toggleClass('open');
-			$('#embedCodeProvider').hide();
+			$('#helpBlock').hide();
 		} else {
 			$(this).toggleClass('open');
-			$('#embedCodeProvider').show();
-			$('#embedCodeProvider textarea').select();
+			$('#helpBlock').show();
+			$('#helpBlock textarea').select();
 		}
 
 
@@ -19,7 +18,7 @@ $(document).ready(function() {
 	});
 	$('body').delegate('#save-question', 'click', function(e) {
 		$(this).siblings('textarea').text('');
-		$('.action-message').text('Bingo! Your question has been added.').fadeIn('fast', function() {
+		$('.action-message').text('Yay! You\'ve added a new question.').fadeIn('fast', function() {
 				$(this).delay(2500).fadeOut('fast');	
 		});;
 
