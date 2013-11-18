e041008c3179c76c80951b7366bfac241dcca4b5
Darren Buckner <darren+git@darrenbuckner.com>
2013-11-17 16:42:13 -0800

    cleanup of question display onsite

--- app/client.js
+++ app/client.js
@@ -65,12 +65,12 @@ $ub.displayMessage = function() {
 	if ( !is_muted ) { 
 
 		var html = ""+
-			"<div id='ub-container' style='z-index: 1000; padding: 20px 10px 15px; height: 50px; position: fixed; bottom: 0; left: 0; right: 0; background-color: #fbfbfb; color: #323232; font-size: 16px; border-top: 5px solid #D8E0E5;'>"+
-			"	<div style='text-align: left; position: relative; max-width: 980px; margin: 0 auto;'>"+
-			"		<span id='message-text' style='text-align: left;padding: 0 150px 0 50px; word-break: break-word; position: absolute; left: 0; right: 45px;'>" + $ub.selectedMessage.text + "</span>"+
+			"<div id='ub-container' style='z-index: 1000; padding: 20px 10px 15px; height: 40px; position: fixed; bottom: 0; left: 0; right: 0; background-color: #fbfbfb; color: #323232; font-size: 16px; border-top: 2px solid #D8E0E5;'>"+
+			"	<div style='text-align: left; position: relative; max-width: 1024px; margin: 0 auto;'>"+
+			"		<span id='message-text' style='text-align: left;padding: 5px 150px 0 0; word-break: break-word; position: absolute; left: 0; right: 45px;'>" + $ub.selectedMessage.text + "</span>"+
 			"		<span style='position: absolute; right: 0;'>"+
-			"			<a style='text-align: center; background-color: #2ecc71; color: #ffffff; text-decoration: none; padding: 5px 10px; width: 60px; display: inline-block; border-radius: 6px' href='' id='ub-yes'>Yes</a> "+
-			"			<a style='text-align: center; background-color: #2ecc71; color: #ffffff; text-decoration: none; padding: 5px 10px; width: 60px; display: inline-block; border-radius: 6px' href='' id='ub-no'>No</a>"+
+			"			<a style='text-align: center; background-color: #2ecc71; color: #ffffff; text-decoration: none; padding: 5px 10px; width: 60px; display: inline-block;' href='' id='ub-yes'>Yes</a> "+
+			"			<a style='text-align: center; background-color: #2ecc71; color: #ffffff; text-decoration: none; padding: 5px 10px; width: 60px; display: inline-block;' href='' id='ub-no'>No</a>"+
 			"		</span>"+
 			"	</div>"+
 			"   <div style='position: absolute; bottom: 5px; left: 10px; font-size:10px'><a href='' id='ub-mute'>Don't show this again</a></div>"+
