2d68bb4985efcebc8d1cc49f1dbdb552e7a72467
Darren Buckner <darren+git@darrenbuckner.com>
2013-12-14 16:23:46 -0800

    admin area cleanup and polish

--- app/scripts/ui-interactions.js
+++ app/scripts/ui-interactions.js
@@ -4,10 +4,15 @@ $(document).ready(function() {
 		if ($(this).hasClass('open')) {
 			$(this).toggleClass('open');
 			$('#helpBlock').hide();
+			$('.ask-new-question').removeClass('highlight');
+			$('#questionsWrap').show();
 		} else {
+			$('#settingsBlock').hide();
 			$(this).toggleClass('open');
 			$('#helpBlock').show();
 			$('#helpBlock textarea').select();
+			$('.ask-new-question').addClass('highlight');
+			$('#questionsWrap').hide();
 		}
 	});
 	$('body').delegate('#settingsBlockShow', 'click', function(e) {
@@ -17,13 +22,19 @@ $(document).ready(function() {
 			$('#settingsBlock').hide();
 		} else {
 			$(this).toggleClass('open');
+			$('#helpBlock').hide();
+			$('.ask-new-question').removeClass('highlight');
 			$('#settingsBlock').show();
+			$('#questionsWrap').show();
 		}
 	});
 	$('body').delegate('.close', 'click', function(e) {
 		e.preventDefault();
-		$(this).parent().hide();
+		$('#settingsBlock').hide();
+		$('#helpBlock').hide();
 		$('#helpBlockShow').toggleClass('open');
+		$('.ask-new-question').removeClass('highlight');
+		$('#questionsWrap').show();
 	});
 	$('body').delegate('#save-question', 'click', function(e) {
 		$(this).siblings('textarea').text('');
