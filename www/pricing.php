4b139170960b1d1d33817900235dbb5687c2642f
statecollege <davesmccarthy@gmail.com>
2014-01-30 14:34:59 -0800

    added pricing page

---
+++ www/pricing.php
@@ -0,0 +1,24 @@
+<?php
+	include:('inc/environment.php');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+			    <!-- Basic Page Needs
+	    ================================================== -->
+    	<meta charset="utf-8">
+	    <title>UserBallot</title>
+    	<meta name="description" content="">
+    	<meta name="author" content="">
+
+	    <?php
+    	    include('inc/head.php');
+	    ?>
+	</head>
+	<body>
+		<?php
+        include('inc/topnav.php');
+    	?>
+		<div></div>
+	</body>
+</html>
